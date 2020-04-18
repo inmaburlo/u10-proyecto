527d9f4f45f51d70f13fd8d065435748f0ce531c
Inma_Burlo <inmaburlo@gmail.com>
2020-04-18 12:10:08 +0200

    Añadida ampliación con la imagen que muestra el resultado correcto

--- src/IBDRFunciona.java
+++ src/IBDRFunciona.java
@@ -9,16 +9,7 @@ public class IBDRFunciona {
 		
 		System.out.println("Introduce un número: ");
 		numero = entrada.nextLine();
-		/**
-		 * Si es un número de una sola cifra (no negativo), llamamos al método mostrar, que muestra
-		 * por pantalla numero = numero, si no, llamamos al método suma que mostrará, si es un número 
-		 * negativo, null, y si no, la suma de los digitos que componen el número.
-		 */
-		if(Integer.parseInt(numero) < 10 && Integer.parseInt(numero) > 0) {
-			System.out.println("Resultado: " + sumi.mostrar(numero));
-		}else {
-			System.out.println("Resultado: " + sumi.sumar(numero));
-		}
+		System.out.println("Resultado: " + sumi.mostrar(numero));
 	}
 
 }
