466f223fc575f8b18c90fb892ea63945eed86508
Inma_Burlo <inmaburlo@gmail.com>
2020-04-18 09:56:22 +0200

    preparando prueba dos

---
+++ test/testValorNegativo.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testValorNegativo {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
